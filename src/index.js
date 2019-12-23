8039a93a523c2c654468fa5edf251e0f8fb8268b
Marcos Felipe <p.marcosfelipe@gmail.com>
2019-12-22 21:18:31 -0300

    Multiple componentes in one file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import FirstComponent from '../src/components/FirstComponent';
+// import FirstComponent from '../src/components/FirstComponent';
+import { CompA, CompB as B } from './components/TwoComponents';
 
 const element = document.getElementById('root');
-ReactDOM.render(<FirstComponent valor="Testing Props" />, element);
+ReactDOM.render(
+  <div>
+    {/* <FirstComponent valor="Testing Props" />  */}
+    <CompA valor="Componente A!" />
+    <B valor="Componente B!" />
+  </div>,
+  element
+);
