7aceb59e8d0b3c23775275720c30ee7a4e9d3c2a
Marcos Felipe <p.marcosfelipe@gmail.com>
2020-01-02 20:43:21 -0300

    Relation between components

---
+++ src/components/FamiliaSilva.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Membro from './Membro';
+
+export default props => (
+  <div>
+    <Membro name={'João'} surname={'Silva'}></Membro>
+    <Membro name={'Paulo'} surname={'Silva'}></Membro>
+    <Membro name={'Pedro'} surname={'Silva'}></Membro>
+    <Membro name={'Ana'} surname={'Silva'}></Membro>
+  </div>
+);
--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom';
 
 // import FirstComponent from '../src/components/FirstComponent';
 // import { CompA, CompB as B } from './components/TwoComponents';
-import MultiElements from './components/MultiElements';
+// import MultiElements from './components/MultiElements';
+import FamiliaSilva from './components/FamiliaSilva';
 
 const element = document.getElementById('root');
 ReactDOM.render(
   <div>
+    <FamiliaSilva />
     {/* <FirstComponent valor="Testing Props" /> 
     <CompA valor="Componente A!" />
     <B valor="Componente B!" /> */}
-    <MultiElements />
+    {/* <MultiElements /> */}
   </div>,
   element
 );
