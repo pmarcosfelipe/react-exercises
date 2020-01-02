8667d6c8fb7b9f573299811c1024042fba1b6194
Marcos Felipe <p.marcosfelipe@gmail.com>
2020-01-02 20:47:00 -0300

    Relation between components

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import ReactDOM from 'react-dom';
 // import FirstComponent from '../src/components/FirstComponent';
 // import { CompA, CompB as B } from './components/TwoComponents';
 // import MultiElements from './components/MultiElements';
-import FamiliaSilva from './components/FamiliaSilva';
+// import FamiliaSilva from './components/FamiliaSilva';
+import Familia from './components/Familia';
+import Membro from './components/Membro';
 
 const element = document.getElementById('root');
 ReactDOM.render(
   <div>
-    <FamiliaSilva />
+    <Familia>
+      <Membro name="João" surname="Pereira"></Membro>
+      <Membro name="Marcos " surname="Pereira"></Membro>
+    </Familia>
+    {/* <FamiliaSilva /> */}
     {/* <FirstComponent valor="Testing Props" /> 
     <CompA valor="Componente A!" />
     <B valor="Componente B!" /> */}
