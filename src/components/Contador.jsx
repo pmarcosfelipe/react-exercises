618b9732131f405708bf61e415b5873a107e62fa
Marcos Felipe <p.marcosfelipe@gmail.com>
2020-01-05 16:03:15 -0300

    Using state and setState to change number state

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,23 +1,30 @@
 import React, { Component } from 'react';
 
 export default class Contador extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.incrementar = this.incrementar.bind(this);
-  // }
+  state = {
+    numero: 0
+  };
 
-  incrementar() {
-    console.log(this);
-    // this.props.numero++;
-  }
+  incrementar = () => {
+    this.setState({ numero: this.state.numero + 1 });
+  };
+
+  decrementar = () => {
+    this.setState({ numero: this.state.numero - 1 });
+  };
 
   render() {
     return (
       <div>
-        <div>Número: {this.props.numero}</div>
-        <button onClick={() => this.incrementar()}>Inc</button>
-        <button>Dec</button>
+        <div>Número: {this.state.numero}</div>
+        <button onClick={this.incrementar}>Inc</button>
+        <button onClick={this.decrementar}>Dec</button>
       </div>
     );
   }
 }
+
+// constructor(props) {
+//   super(props);
+//   this.incrementar = this.incrementar.bind(this);
+// }
