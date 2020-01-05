cae1b20e40a3889bfca782019515331f6720de2c
Marcos Felipe <p.marcosfelipe@gmail.com>
2020-01-05 15:57:16 -0300

    Fix "this" issue using arrow function

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 
 export default class Contador extends Component {
-  constructor(props) {
-    super(props);
-    this.incrementar = this.incrementar.bind(this);
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.incrementar = this.incrementar.bind(this);
+  // }
 
   incrementar() {
-    this.props.numero++;
+    console.log(this);
+    // this.props.numero++;
   }
 
   render() {
     return (
       <div>
         <div>Número: {this.props.numero}</div>
-        <button onClick={this.incrementar}>Inc</button>
+        <button onClick={() => this.incrementar()}>Inc</button>
         <button>Dec</button>
       </div>
     );
