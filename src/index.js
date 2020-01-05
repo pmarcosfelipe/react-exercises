e0302c617c6bcda1d309a100eddf18e4f3baad0a
Marcos Felipe <p.marcosfelipe@gmail.com>
2020-01-05 14:33:06 -0300

    Indirect comunication between father and son components

--- src/index.js
+++ src/index.js
@@ -5,17 +5,20 @@ import ReactDOM from 'react-dom';
 // import { CompA, CompB as B } from './components/TwoComponents';
 // import MultiElements from './components/MultiElements';
 // import FamiliaSilva from './components/FamiliaSilva';
-import Familia from './components/Familia';
-import Membro from './components/Membro';
+// import Familia from './components/Familia';
+// import Membro from './components/Membro';
+// import ComponentWithFunction from './components/ComponentWithFunction';
+import Pai from './components/Pai';
 
 const element = document.getElementById('root');
 ReactDOM.render(
   <div>
-    <Familia surname="Pereira">
+    <Pai />
+    {/* <ComponentWithFunction /> */}
+    {/* <Familia surname="Pereira">
       <Membro name="João"></Membro>
       <Membro name="Marcos"></Membro>
-    </Familia>
-
+    </Familia> */}
     {/* <FamiliaSilva /> */}
     {/* <FirstComponent valor="Testing Props" /> 
     <CompA valor="Componente A!" />
